c0983dffd381f9a5f00e88e3a450ec4fddedbfa0
Steven Cassidy <steven.cassidy@gmail.com>
2016-02-08 21:12:32 -0500

    Add some faker seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,27 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 User.create!(username:'steven', password: 'pw123456', password_confirmation:'pw123456', email:'steven@example.com')
-User.create!(username:'kb', password: 'pw123456', password_confirmation:'pw123456', email:'kb@example.com')
-User.create!(username:'kristie', password: 'pw123456', password_confirmation:'pw123456', email:'kristie@example.com')
 
-Restaurant.create!(creator: User.last, name: 'Chez Kristie ', cuisine: 'Modern American', address: '48 Wall St', city: 'New York', state: 'NY', zip:'10005' )
+50.times do
+  User.create!(username:Faker::Internet.user_name, email:Faker::Internet.safe_email, password: 'pw123456', password_confirmation:'pw123456')
+end
+users = User.all
+cuisines = ['American', 'Thai', 'French', 'Italian', 'Japanese', 'Mexican', 'Spanish', 'Vietnamese', 'Chinese']
+
+20.times do
+  Restaurant.create!(
+    name: Faker::Company.name,
+    creator: users.sample,
+    cuisine:cuisines.sample,
+    address: Faker::Address.street_address,
+    city: Faker::Address.city,
+    state: Faker::Address.state,
+    zip: Faker::Address.zip
+   )
+end
+restaurants = Restaurant.all
+ratings = [1,2,3,4,5]
+
+140.times do
+  Review.create!(reviewer: users.sample, restaurant: restaurants.sample, rating: ratings.sample, body:Faker::Hipster.paragraph)
+end
