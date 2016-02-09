6b25017021c67d41eb03bd2ca364df83d9320cd5
Steven Cassidy <steven@mumsnet.com>
2016-02-09 14:54:17 -0500

    Create an example feature spec

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -5,7 +5,19 @@ RSpec.feature 'visitor on topics list page' do
     scenario 'sees a list of topics' do
       r = FactoryGirl.create(:restaurant)
       visit root_path
-      expect(page).to have_content('What!')
+      expect(page).to have_content('Restaurants')
+      expect(page).to have_selector '[data-purpose="header"]'
+      expect(page).to have_content(r.name)
+      expect(page.current_path).to eq('/')
+    end
+
+    scenario 'can click a restaurant to see its details', js: true do
+      # This test doesn't need to be js: true
+      # but having it so lets you see the browser fire up
+      r = FactoryGirl.create(:restaurant)
+      visit root_path
+      click_link(r.name)
+      expect(page.current_path).to eq(restaurant_path(r))
     end
   end
 end
